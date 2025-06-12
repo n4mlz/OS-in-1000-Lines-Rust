413d116ff0feca56bd8df066317f29a8222f2ee1
n4mlz <n4mlz.dev@gmail.com>
2025-06-13 00:57:33 +0900

    :sparkles: SBI 呼び出しを実装

--- src/kernel.rs
+++ src/kernel.rs
@@ -9,6 +9,37 @@ unsafe extern "C" {
     static __stack_top: u8;
 }
 
+#[allow(clippy::too_many_arguments)]
+fn sbi_call(
+    arg0: usize,
+    arg1: usize,
+    arg2: usize,
+    arg3: usize,
+    arg4: usize,
+    arg5: usize,
+    fid: usize,
+    eid: usize,
+) -> Result<usize, isize> {
+    let mut err: isize;
+    let mut value: usize;
+
+    unsafe {
+        asm!(
+            "ecall",
+            inout("a0") arg0 => err,
+            inout("a1") arg1 => value,
+            in("a2") arg2,
+            in("a3") arg3,
+            in("a4") arg4,
+            in("a5") arg5,
+            in("a6") fid,
+            in("a7") eid,
+        );
+    }
+
+    if err < 0 { Err(err) } else { Ok(value) }
+}
+
 #[unsafe(no_mangle)]
 #[unsafe(link_section = ".text.boot")]
 pub extern "C" fn boot() -> ! {
