271f81b61dd7b5d22cac1d21bc0a9479b017d063
n4mlz <n4mlz.dev@gmail.com>
2025-07-02 16:42:47 +0900

    :sparkles: コンテキストスイッチを実装

--- src/process.rs
+++ src/process.rs
@@ -1,4 +1,9 @@
-use crate::constants::KERNEL_STACK_SIZE;
+use core::arch::naked_asm;
+
+use crate::{
+    constants::KERNEL_STACK_SIZE,
+    utils::{Addr, VirtAddr},
+};
 
 #[derive(Clone, Copy, PartialEq)]
 enum State {
@@ -62,5 +67,109 @@ impl Process {
             context: Context::new(),
             stack: [0; KERNEL_STACK_SIZE],
         }
+    }
+}
+
+struct ProcessManager {
+    procs: [Process; 8],
+    current: usize,
+}
+
+impl ProcessManager {
+    fn new() -> Self {
+        let idle_idx = 0;
+
+        let mut pm = ProcessManager {
+            procs: [Process::new(); 8],
+            current: idle_idx,
+        };
+
+        let proc = &mut pm.procs[idle_idx];
+        proc.pid = 0;
+        proc.state = State::Runnable;
+
+        pm
+    }
+
+    fn crate_process(&mut self, pc: VirtAddr) -> Option<u32> {
+        let idx = self.procs.iter().position(|p| p.state == State::Unused)?;
+        let proc = &mut self.procs[idx];
+
+        proc.pid = idx as u32 + 1;
+        proc.state = State::Runnable;
+        proc.context.ra = pc.as_usize();
+        proc.context.sp = proc.stack.as_ptr() as usize + KERNEL_STACK_SIZE;
+
+        Some(proc.pid)
+    }
+
+    #[unsafe(naked)]
+    #[repr(align(4))]
+    unsafe extern "C" fn switch_context(old: &mut Context, new: &Context) {
+        naked_asm!(
+            "
+            sw ra,  4 * 0(a0)
+            sw sp, 4 * 1(a0)
+            sw s0, 4 * 2(a0)
+            sw s1, 4 * 3(a0)
+            sw s2, 4 * 4(a0)
+            sw s3, 4 * 5(a0)
+            sw s4, 4 * 6(a0)
+            sw s5, 4 * 7(a0)
+            sw s6, 4 * 8(a0)
+            sw s7, 4 * 9(a0)
+            sw s8, 4 * 10(a0)
+            sw s9, 4 * 11(a0)
+            sw s10, 4 * 12(a0)
+            sw s11, 4 * 13(a0)
+
+            lw ra,  4 * 0(a1)
+            lw sp, 4 * 1(a1)
+            lw s0, 4 * 2(a1)
+            lw s1, 4 * 3(a1)
+            lw s2, 4 * 4(a1)
+            lw s3, 4 * 5(a1)
+            lw s4, 4 * 6(a1)
+            lw s5, 4 * 7(a1)
+            lw s6, 4 * 8(a1)
+            lw s7, 4 * 9(a1)
+            lw s8, 4 * 10(a1)
+            lw s9, 4 * 11(a1)
+            lw s10, 4 * 12(a1)
+            lw s11, 4 * 13(a1)
+
+            ret
+            "
+        )
+    }
+
+    fn switch(&mut self) {
+        let current = self.current;
+        let next = self
+            .procs
+            .iter()
+            .enumerate()
+            .find(|&(i, proc)| i != current && proc.pid != 0 && proc.state == State::Runnable)
+            .map(|(i, _)| i);
+
+        if next.is_none() && self.procs[current].state == State::Runnable {
+            return;
+        }
+
+        let next = next.unwrap_or(0);
+
+        let (current_proc, next_proc) = if current < next {
+            let (left, right) = self.procs.split_at_mut(next);
+            (&mut left[current], &right[0])
+        } else {
+            let (left, right) = self.procs.split_at_mut(current);
+            (&mut left[0], &right[next])
+        };
+
+        unsafe {
+            Self::switch_context(&mut current_proc.context, &next_proc.context);
+        }
+
+        self.current = next;
     }
 }
